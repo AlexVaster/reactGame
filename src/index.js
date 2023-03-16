ce31e9b4ecf176aa3e611aea13940aa20e2cf5a3
AlexVaster <alexvaster@icloud.com>
2023-03-16 18:03:08 +0300

    Working game but game continues after winning

--- src/index.js
+++ src/index.js
@@ -1,46 +1,108 @@
-import ReactDOM from "react-dom";
-import styles from './Field.module.css';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import "./App.css";
+import { useState } from "react";
 
-const Label = (props) => {
-    return <h1>Next player: {props.name}</h1>;
-};
+// Массив комбинаций
+const winCombo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
-const Square = (props) => {
-    return <button className={styles.square}>{props.value}</button>;
-};
-
-const Field = (props) => {
+// Статус игры
+const GameStatus = ({player, gameFinished, draw }) => {
     return (
-        <div className={styles.field}>
-            <div className="game-status"></div>
-            <div className={styles.fieldRow}>
-                <Square value={0}></Square>
-                <Square value={1}></Square>
-                <Square value={2}></Square>
-            </div>
-            <div className={styles.fieldRow}>
-                <Square value={3}></Square>
-                <Square value={4}></Square>
-                <Square value={5}></Square>
-            </div>
-            <div className={styles.fieldRow}>
-                <Square value={6}></Square>
-                <Square value={7}></Square>
-                <Square value={8}></Square>
-            </div>
-        </div>
+        <div className="status-screen">
+            <span>{gameFinished ? (draw ? 'Ничья!' : 'Победил игрок: ' + (!player ? 'X' : 'O')) 
+                : ('Ход игрока: ' + (player ? 'X' : 'O'))}
+            </span>
+        </div>);
+  };
+
+// Одна ячейка
+const Square = ({ clickedArray, handleClick }) => {
+    return (
+    <div className="board">
+        {clickedArray.map((item, index) => {
+            if (item === null) {
+                return (
+                <div key={index} className="square" onClick={() => handleClick(index)}>{item}</div>);
+            } else {
+                return (
+                <div key={index} className="square clicked">{item}</div>);
+            }
+            })}
+    </div>);
+};
+
+// Поле игры
+const Game = () => {
+    // Массив ячеек игрового поля
+    const [squareArr, setSquareArr] = useState(Array(9).fill(null));
+    // Какой игрок ходит: O == false, X == true
+    const [player, setPlayer] = useState(false);
+    // Статус завершённости игры
+    const [gameFinished, setGameFinished] = useState(false);
+    // Была ли ничья
+    const [draw, setDraw] = useState(false);
+    
+    // При нажатии проверяем по циклу, находим id === index'у ячейки и возврщаем X или Y
+    // В ином случае неизменённую ячейку item
+    // Меняем игрока на другого
+    const handleClick = (id) => {
+        setSquareArr(
+            squareArr.map((item, index) => {
+                return index === id ? (player ? 'X' : 'O') : item;
+            })
+        );
+        setPlayer(!player);
+    };
+
+
+    const isGameOver = () => {
+        if (!gameFinished) {
+            // Перебор победных комбинаций
+            for (let i = 0; i < winCombo.length; i++) {
+                const [a, b, c] = winCombo[i];
+                if (squareArr[a] && squareArr[a] === squareArr[b] && squareArr[a] === squareArr[c]){
+                    setGameFinished(true);
+                    return squareArr[a];
+                }
+            }
+            // Проверка на ничью
+            if (!squareArr.includes(null)) {
+                setDraw(true);
+                setGameFinished(true);
+                return null;
+            }
+        }
+        return null;
+    };
+
+    isGameOver();
+
+    return (
+    <div className="game">
+        <Square clickedArray={squareArr} handleClick={handleClick} />
+        <GameStatus player={player} gameFinished={gameFinished} draw={draw}/>
+    </div>
     );
 };
 
+// Всё приложение
 const App = () => {
     return (
-        <div>
-            <Label></Label>
-            <Field></Field>
+    <div className="App">
+        <Game />
         </div>
-    )
-};
+        );
+    };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const app = <App></App>;
-root.render(app);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
