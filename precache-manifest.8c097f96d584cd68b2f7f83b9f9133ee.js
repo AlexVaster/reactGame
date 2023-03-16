0836a74be9bcbffb68809e9d4238ce49fca15b9d
AlexVaster <alexvaster@icloud.com>
2023-03-16 18:24:39 +0300

    Updates

---
+++ precache-manifest.8c097f96d584cd68b2f7f83b9f9133ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "653ed844102c4589a6db",
+    "url": "/reactGame/static/css/main.cccadfde.chunk.css"
+  },
+  {
+    "revision": "653ed844102c4589a6db",
+    "url": "/reactGame/static/js/main.653ed844.chunk.js"
+  },
+  {
+    "revision": "2927c5a2eb8ceb1d6909",
+    "url": "/reactGame/static/js/1.2927c5a2.chunk.js"
+  },
+  {
+    "revision": "b9fb290e6bd9fcbdc29b",
+    "url": "/reactGame/static/js/runtime~main.b9fb290e.js"
+  },
+  {
+    "revision": "c2323301d9bc2288e1918621ce9f45c0",
+    "url": "/reactGame/index.html"
+  }
+];
